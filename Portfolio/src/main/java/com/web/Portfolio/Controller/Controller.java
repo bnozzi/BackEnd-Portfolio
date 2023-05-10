ae75d78c22b1326137f0bb002b78e8e8c214e0a3
Bruno Pizzi <b.pizzipe@gmail.com>
2023-05-10 00:34:24 -0300

    w

--- Portfolio/src/main/java/com/web/Portfolio/Controller/Controller.java
+++ Portfolio/src/main/java/com/web/Portfolio/Controller/Controller.java
@@ -11,31 +11,32 @@ import com.web.Portfolio.Service.ProyectosService;
 import com.web.Portfolio.Service.SobreMiService;
 import com.web.Portfolio.Service.UserService;
 
-import java.util.ArrayList;
+import io.jsonwebtoken.JwtException;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 // en la clase controller tengo que crear todas las rest apis o rutas para responder a las peticiones 
 @RestController
+@RequestMapping("/")
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
 public class Controller {
-
-    private List<Usuario> personas = new ArrayList<Usuario>();
-
-    /*
-     * Test getmapping
-     */
-    @GetMapping("/hola")
-    public String sayHelloString() {
-        return "hello world";
-    }
 
     // userService
 
@@ -58,84 +59,32 @@ public class Controller {
     @Autowired
     private SobreMiService sobreMiService;
 
+    @Autowired
+    private AuthUser auth;
+
     @PostMapping("/aboutMe")
     public void aboutMe(@RequestBody SobreMi sobreMi) {
         // ! delete this method
         sobreMiService.addSobreMi(sobreMi);
     }
 
-    @GetMapping("/aboutMe/{id}")
-    public SobreMi getSobreMi(@PathVariable Long id) {
-        return sobreMiService.getAboutMe(id);
+    @GetMapping("/aboutMe")
+    public SobreMi getSobreMi() {
+        return sobreMiService.getAboutMe();
     }
 
-    // Education service
-    @Autowired
-    private EducacionService educacionService;
-
-    @PostMapping("/addEducation")
-    public void addEducation(@RequestBody Educacion educacion) {
-        educacionService.addEducation(educacion);
+    @PutMapping("/updateAboutMe")
+    public ResponseEntity<Map<String, String>> updateAboutMe(@RequestBody SobreMi sobreMi,
+            @RequestHeader("Authorization") String authorizationHeader) {
+        Map<String, String> response = new HashMap<>();
+        String jwt = authorizationHeader.substring(7);
+        if (auth.isJwtValid(jwt, auth.getSecretKey())) {
+            sobreMiService.editAboutMe(sobreMi);
+            response.put("mensaje", "Sobre mí actualizado");
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } else {
+            throw new JwtException("JWT no válido");
+        }
     }
-
-    @GetMapping("/getEducation/{id}")
-    public Educacion getEducation(@PathVariable Long id) {
-        return educacionService.getEducation(id);
-
-    }
-
-    @GetMapping("/getAllEducation")
-    public List<Educacion> getEducations() {
-        return educacionService.getAllEducations();
-    }
-
-    @DeleteMapping("/deleteEducation/{id}")
-    public void deleteEducation(@PathVariable Long id) {
-        educacionService.removeEducation(id);
-
-    }
-
-    @PutMapping("/updateEducation/{id}")
-    public void updateEducation(@PathVariable Long id, @RequestBody Educacion educacion) {
-
-        educacionService.updateEducation(id, educacion);
-    }
-
-    // Proyect service
-    @Autowired
-    private ProyectosService proyectoService;
-
-    // get mapping proyect by id
-    @GetMapping("/getAllProyects")
-    public List<Proyecto> getProyects() {
-        return proyectoService.getAllProyects();
-    }
-
-    //get by id 
-    @GetMapping("getProyect/{id}")
-    public Proyecto getProyectById(@PathVariable Long id) {
-        return proyectoService.getProyect(id);
-    }
-
-    @PostMapping("/addProyect")
-    public void addProyect(@RequestBody Proyecto proyecto) {
-        proyectoService.addProyect(proyecto);
-    }
-
-    @DeleteMapping("/deleteProyect/{id}")
-    public void deleteProyect(@PathVariable Long id) {
-        proyectoService.deleteProyect(id);
-    }
-
-    @PutMapping("/updateProyect/{id}")
-    public void updateProyect(@PathVariable Long id, @RequestBody Proyecto proyecto) {
-        proyectoService.updateProyect(id, proyecto);
-    }
-
-
-    
 
 }
-    
-
-
---
+++ Portfolio/src/main/java/com/web/Portfolio/Controller/EducacionController.java
@@ -0,0 +1,89 @@
+package com.web.Portfolio.Controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.web.Portfolio.Model.Educacion;
+import com.web.Portfolio.Service.EducacionService;
+
+import io.jsonwebtoken.JwtException;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class EducacionController {
+
+    @Autowired
+    private EducacionService educacionService;
+
+    @Autowired
+    private AuthUser auth;
+
+    @PostMapping("/addEducation")
+    public ResponseEntity<Map<String, String>> addEducation(@RequestBody Educacion educacion,
+            @RequestHeader("Authorization") String authorizationHeader) {
+        Map<String, String> response = new HashMap<>();
+        String jwt = authorizationHeader.substring(7);
+        if (auth.isJwtValid(jwt, auth.getSecretKey())) {
+            educacionService.addEducation(educacion);
+            response.put("mensaje", "Educacion agregada");
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } else {
+            throw new JwtException("JWT no válido");
+        }
+    }
+
+    @GetMapping("/getEducation/{id}")
+    public Educacion getEducation(@PathVariable Long id) {
+        return educacionService.getEducation(id);
+    }
+
+    @GetMapping("/getAllEducation")
+    public List<Educacion> getEducations() {
+        return educacionService.getAllEducations();
+    }
+
+    @DeleteMapping("/deleteEducation/{id}")
+    public ResponseEntity<Map<String, String>> deleteEducation(@PathVariable Long id,
+            @RequestHeader("Authorization") String authorizationHeader) {
+        Map<String, String> response = new HashMap<>();
+        String jwt = authorizationHeader.substring(7);
+        if (auth.isJwtValid(jwt, auth.getSecretKey())) {
+            educacionService.removeEducation(id);
+            response.put("mensaje", "educacion eliminada");
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } else {
+            throw new JwtException("JWT no válido");
+        }
+    }
+
+    @PutMapping("/updateEducation/{id}")
+    public ResponseEntity<Map<String, String>> updateEducation(@PathVariable Long id, @RequestBody Educacion educacion,
+            @RequestHeader("Authorization") String authorizationHeader) {
+        Map<String, String> response = new HashMap<>();
+        String jwt = authorizationHeader.substring(7);
+        if (auth.isJwtValid(jwt, auth.getSecretKey())) {
+            educacionService.updateEducation(id, educacion);
+            response.put("mensaje", "educacion actualizada");
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } else {
+            throw new JwtException("JWT no válido");
+        }
+    }
+
+}
