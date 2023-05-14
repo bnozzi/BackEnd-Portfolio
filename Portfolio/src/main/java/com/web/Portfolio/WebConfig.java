25f933d53520823fc88de2680758fcfc358e3ebb
Bruno Pizzi <b.pizzipe@gmail.com>
2023-05-14 16:04:14 -0300

    allowedOrigins

--- Portfolio/src/main/java/com/web/Portfolio/WebConfig.java
+++ Portfolio/src/main/java/com/web/Portfolio/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer{
     @Override
 public void addCorsMappings(CorsRegistry registry) {
 registry.addMapping("/**").
-allowedMethods("POST", "PUT", "DELETE", "OPTIONS")
-.allowedHeaders("*");
+allowedMethods("GET","POST", "PUT", "DELETE", "OPTIONS")
+.allowedHeaders("*")                
+.allowedOrigins("*"); // permitir todos los orígenes
 }
 }
